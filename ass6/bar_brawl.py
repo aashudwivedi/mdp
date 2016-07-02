968b04ad73bc0b99c074200534a1c41c8f432626
Ashutosh Dwivedi <adwivedi@akamai.com>
2016-07-02 18:52:45 +0530

    init commit to read input

---
+++ ass6/bar_brawl.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def string_to_np(string):
+    string = string.replace('{', '[')
+    string = string.replace('}', ']')
+    input_list = eval(string)
+    return np.asarray(input_list, dtype=int)
+
+
+def process_test_case(patron_count, establishment, fight_occured):
+    patron_count = int(patron_count)
+
+
+def read_input_and_solve():
+    f = open('input.txt')
+
+    try:
+        while True:
+            patron_count = int(f.readline())
+            establishment = string_to_np(f.readline())
+            fight_occured = string_to_np(f.readline())
+
+            print patron_count
+            print establishment
+            print fight_occured
+    except ValueError:
+        pass
+
+if __name__ == '__main__':
+    read_input_and_solve()
